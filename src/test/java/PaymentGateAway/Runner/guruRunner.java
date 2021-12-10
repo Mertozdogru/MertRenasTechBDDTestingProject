a67af94d1f72b3f6c062c8c4e5e190edfb20956c
mertozdogru <mertozdogruu@gmail.com>
2021-12-10 14:13:34 -0500

    log4j

--- src/test/java/PaymentGateAway/Runner/guruRunner.java
+++ src/test/java/PaymentGateAway/Runner/guruRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         plugin ={  "pretty",
                 "html:target/HTMLReports/reports.html",
                 "json:target/JSONReports/report.json",
-               // "junit:target/JUNITReports/report.xml",
+                "junit:target/JUNITReports/report.xml",
                 "rerun:target/rerun.txt"
         },
         publish=true,
